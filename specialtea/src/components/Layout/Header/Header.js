1c5df73b597cf6fdff7719b45f9bfcd6dae0f52a
Kyle Buttress <kylebuttress11@gmail.com>
2021-05-06 23:45:45 +0200

    Header component JSX

---
+++ specialtea/src/components/Layout/Header/Header.js
@@ -0,0 +1,23 @@
+import { Fragment } from 'react';
+
+const Header = props => {
+	return (
+		<Fragment>
+			<header className={}>
+				<h2 className={}>
+					<span className={}>Special</span>
+					<span className={}>Tea</span>
+				</h2>
+				<div>
+					<img className={} src={} alt={} />
+				</div>
+				<button>Cart</button>
+			</header>
+			<div className={}>
+				<img className={} src={} alt={} />
+			</div>
+		</Fragment>
+	);
+};
+
+export default Header;
