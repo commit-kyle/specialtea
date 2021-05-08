91676bec358cc2533e9f95e778bb33f5cb3de6dd
Kyle Buttress <kylebuttress11@gmail.com>
2021-05-08 16:24:18 +0200

    Renders a list of teas, currently set to dummy info

---
+++ specialtea/src/components/Teas/AvailableTeas/AvailableTeas.js
@@ -0,0 +1,50 @@
+import classes from './AvailableTeas.module.css';
+
+const DUMMY_TEAS = [
+	{
+		id: 't1',
+		name: 'Darjeeling',
+		type: 'Black tea',
+		description:
+			'Darjeeling is the top Indian tea. It has a light, nutty flavor as compared to the bold and robust taste of other black teas.',
+		price: 22.99,
+	},
+	{
+		id: 't2',
+		name: 'English Breakfast',
+		type: 'Black tea',
+		description:
+			'Perhaps unsurprisingly, English Breakfast is the most popular tea in England. Much of this is a blend of Assam, Ceylon and Kenyan black tea',
+		price: 16.50,
+	},
+	{
+		id: 't3',
+		name: 'Matcha',
+		type: 'Green tea',
+		description:
+			'Green tea has been found to have a high concentration of antioxidants and polyphenols. Harvested, withered and dried immediately to prevent oxidation and retain its natural green color and nutrients.',
+		price: 12.99,
+	},
+	{
+		id: 't4',
+		name: 'Chai',
+		type: 'Black',
+		description:
+			'This milky, sugary, spiced beverage is drunk all day long in India. Loaded with spices like ginger, cinnamon, cardamom, and cloves; and sweetened with honey.',
+		price: 18.99,
+	},
+];
+
+const AvailableTeas = () => {
+	const teaList = DUMMY_TEAS.map(tea => {
+		return <li>{tea.name}</li>;
+	});
+
+	return (
+		<section className={classes.teas}>
+			<ul>{teaList}</ul>
+		</section>
+	);
+};
+
+export default AvailableTeas;
