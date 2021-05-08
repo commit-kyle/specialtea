15504d282f437630c81129e3423d0e411fdbfce1
Kyle Buttress <kylebuttress11@gmail.com>
2021-05-08 17:42:21 +0200

    Configured component to accept user input

---
+++ specialtea/src/components/Teas/TeaItem/TeaItemForm/TeaItemForm.js
@@ -0,0 +1,12 @@
+import classes from './TeaItemForm.module.css';
+
+const TeaItemForm = props => {
+	return (
+		<form className={classes.form}>
+			<input />
+			<button>+ Add</button>
+		</form>
+	);
+};
+
+export default TeaItemForm;
