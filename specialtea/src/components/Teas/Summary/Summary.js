3270b271ce42d4e6a96853203fc23461baba61f9
Kyle Buttress <kylebuttress11@gmail.com>
2021-05-08 16:21:50 +0200

    Component which serves as a welcome for users and a summary of products offered

---
+++ specialtea/src/components/Teas/Summary/Summary.js
@@ -0,0 +1,16 @@
+import classes from './Summary.module.css';
+
+const Summary = () => {
+	return (
+		<section className={classes.summary}>
+			<h2>Special tea is our specialty!</h2>
+			<p>
+				Choose your special tea from our broad selection and enjoy a
+				tasty cup at home.
+			</p>
+      <p>Tea received from us can be enjoyed on-time, knowing that they are of high-quality.</p>
+		</section>
+	);
+};
+
+export default Summary;
