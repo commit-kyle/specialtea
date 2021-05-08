e2d966effd80266400692dab204d6ffc20871db5
Kyle Buttress <kylebuttress11@gmail.com>
2021-05-08 16:26:26 +0200

    Renders all components associated and responsible for rendering tea products

---
+++ specialtea/src/components/Teas/Teas.js
@@ -0,0 +1,13 @@
+import Summary from './Summary/Summary';
+import AvailableTeas from './AvailableTeas/AvailableTeas';
+
+const Teas = () => {
+	return (
+		<main>
+			<Summary />
+			<AvailableTeas />
+		</main>
+	);
+};
+
+export default Teas;
