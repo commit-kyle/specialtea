fb38fc832c77f66be5acf3566c419a61e8306d79
Kyle Buttress <kylebuttress11@gmail.com>
2021-05-08 18:54:19 +0200

    Reconfigured component to accept a unique ID for labels. This fixed an issue where clicking on labels always highlighted first input. Necessary for accessibility

--- specialtea/src/components/Teas/TeaItem/TeaItemForm/TeaItemForm.js
+++ specialtea/src/components/Teas/TeaItem/TeaItemForm/TeaItemForm.js
@@ -1,9 +1,20 @@
+import Input from '../../../UI/Input/Input';
 import classes from './TeaItemForm.module.css';
 
 const TeaItemForm = props => {
 	return (
 		<form className={classes.form}>
-			<input />
+			<Input
+				label={'Quantity'}
+				input={{
+					id: 'Amount_' + props.id,
+					type: 'number',
+					min: '1',
+					max: '5',
+					step: '1',
+					defaultValue: '1',
+				}}
+			/>
 			<button>+ Add</button>
 		</form>
 	);
