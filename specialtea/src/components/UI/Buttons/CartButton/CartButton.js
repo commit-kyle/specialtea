0d35061dd76b0573fa6806ce5ee493d8ade03448
Kyle Buttress <kylebuttress11@gmail.com>
2021-05-08 16:17:20 +0200

    Configured UI button for accessing users cart

--- specialtea/src/components/UI/Buttons/CartButton/CartButton.js
+++ specialtea/src/components/UI/Buttons/CartButton/CartButton.js
@@ -1,13 +1,15 @@
 import CartIcon from '../../../Cart/CartIcon/CartIcon';
 
+import classes from './CartButton.module.css';
+
 const CartButton = () => {
 	return (
-		<button>
-			<span>
+		<button className={classes.button}>
+			<span className={classes.icon}>
 				<CartIcon />
 			</span>
-			<span>Your Cart</span>
-			<span>3</span>
+			<span>Cart</span>
+			<span className={classes.badge}>3</span>
 		</button>
 	);
 };
