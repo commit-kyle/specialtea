8c0fdc1857d8f95a6f29e4f2c7685fa9b190b515
Kyle Buttress <kylebuttress11@gmail.com>
2021-05-09 19:40:07 +0200

    Configured state logic to have an item added to the Cart component as well as the total amount of all items within the Cart

--- specialtea/src/store/CartContext/CartProvider.js
+++ specialtea/src/store/CartContext/CartProvider.js
@@ -1,13 +1,41 @@
+import { useReducer } from 'react';
+
 import CartContext from './cart-context.js';
 
-const CartProvider = props => {
-	const addToCartHandler = item => {};
+const defaultCartState = {
+	items: [],
+	totalAmount: 0,
+};
 
-	const removeFromCarthandler = id => {};
+const cartReducer = (state, action) => {
+	if (action.type === 'ADD') {
+		const updatedItems = state.items.concat(action.item);
+		const updatedTotalAmount =
+			state.totalAmount + action.item.price * action.item.amount;
+
+		return {
+			items: updatedItems,
+			totalAmount: updatedTotalAmount,
+		};
+	}
+
+	return defaultCartState;
+};
+
+const CartProvider = props => {
+	const [cartState, dispatchCart] = useReducer(cartReducer, defaultCartState);
+
+	const addToCartHandler = item => {
+		dispatchCart({ type: 'ADD', item: item });
+	};
+
+	const removeFromCarthandler = id => {
+		dispatchCart({ type: 'REMOVE', id: id });
+	};
 
 	const cartContext = {
-		items: [],
-		totalAmount: 0,
+		items: cartState.items,
+		totalAmount: cartState.totalAmount,
 		addItem: addToCartHandler,
 		removeItem: removeFromCarthandler,
 	};
