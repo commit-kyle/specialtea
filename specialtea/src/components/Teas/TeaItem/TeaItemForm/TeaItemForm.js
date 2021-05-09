82e1e58319d0e5c771050947d8de64fb90b7aac7
Kyle Buttress <kylebuttress11@gmail.com>
2021-05-09 19:56:57 +0200

    Configured function to handle state for providing a validated amount through props. This is to be later forwarded to context

--- specialtea/src/components/Teas/TeaItem/TeaItemForm/TeaItemForm.js
+++ specialtea/src/components/Teas/TeaItem/TeaItemForm/TeaItemForm.js
@@ -1,10 +1,33 @@
+import { useRef, useState } from 'react';
+
 import Input from '../../../UI/Input/Input';
 import classes from './TeaItemForm.module.css';
 
 const TeaItemForm = props => {
+	const [amountIsValid, setAmountIsValid] = useState(true);
+	const amountInputRef = useRef();
+
+	const submitHandler = e => {
+		e.preventDefault();
+
+		const enteredAmount = amountInputRef.current.value;
+		const enteredAmountNum = +enteredAmount;
+
+		if (
+			enteredAmount.trim().length === 0 ||
+			enteredAmountNum < 1 ||
+			enteredAmountNum > 5
+		) {
+			setAmountIsValid(false);
+			return;
+		}
+		props.onAddToCart(enteredAmountNum);
+	};
+
 	return (
-		<form className={classes.form}>
+		<form className={classes.form} onSubmit={submitHandler}>
 			<Input
+				ref={amountInputRef}
 				label={'Quantity'}
 				input={{
 					id: 'Amount_' + props.id,
@@ -15,7 +38,8 @@ const TeaItemForm = props => {
 					defaultValue: '1',
 				}}
 			/>
-			<button>+ Add</button>
+			<button type="submit">+ Add</button>
+			{!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
 		</form>
 	);
 };
